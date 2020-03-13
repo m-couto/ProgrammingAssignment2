9c20c233d996ee395809108fa7429f0162c51107
Miguel Couto <56915301+m-couto@users.noreply.github.com>
2020-03-13 16:22:04 +0100

    My solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow us to save the values of inverses of
+## matrices that have been previously computed.
 
-## Write a short comment describing this function
+## This function returns a list of 4 functions that are needed for
+## the second function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # The argument x is the matrix we want to invert.
+    
+    inv <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    # This allows us to set a new value to the matrix x.
+    
+    get <- function() x
+    # Gives us the value of matrix x.
+    
+    setinv <- function(found) inv <<- found
+    # This will allows us to save the value of the inverse,
+    # once it's computed for the first time.
+    
+    getinv <- function() inv
+    # Gives us the value of inv, which is either NULL (if the
+    # inverse matrix hasn't been calculated yet) or it is the
+    # value of the inverse matrix (once it has been found and 
+    # saved here).
+    
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function makes use of the first function to either compute
+## the inverse of a matrix for the first time and save it, or to  
+## take the previously saved value of the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## The object x must be the list returned by the first function
+    ## on the matrix we want to invert.
+    
+    inv <- x$getinv()
+    
+    # If the inverse has been calculated before
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    
+    # If the inverse hasn't been calculated yet
+    data <- x$get()
+    inv <- solve(data, ...)     # we calculate it
+    x$setinv(inv)               # and we save it
+    inv
 }
